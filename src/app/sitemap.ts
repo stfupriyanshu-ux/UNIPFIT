ad4c151f779fad5b603bc0246c4c663f4e2dce16
stfupriyanshu-ux <stfupriyanshu@gmail.com>
2026-09-21 19:41:08 +0530

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://unipfit.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
+
