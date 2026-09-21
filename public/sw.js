bd5a448a64862ac1128f06df65ff7549b198d767
stfupriyanshu-ux <stfupriyanshu@gmail.com>
2026-09-21 22:26:35 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Minimal service worker: installable app + offline fallback page. API calls are never cached.
-const CACHE = 'univzero-v1';
+const CACHE = 'unipfit-v1';
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['/offline', '/icons/icon-192.png']))); self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', (e) => {
